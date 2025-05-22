6dad43fd2c076d35f02daa46eed1b8feab303976
Ace120C <145904614+Ace120C@users.noreply.github.com>
2025-05-22 13:38:45 +0100

    added print statements to figure out X/O pos

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <raylib.h>
+#include <stdio.h>
 
 int main()
 {
   // Init 
   InitWindow(800, 800, "tictactoe");
   Texture2D Board = LoadTexture("./assets/board.png");
+  SetTargetFPS(50);
   // Game loop
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawTexture(Board, 0, 0, RAYWHITE);
     EndDrawing();
+
+    int MousePosX = GetMouseX();
+    int MousePosY = GetMouseY();
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      printf("X: %d\n", MousePosX);
+      printf("Y: %d\n", MousePosY);
+    }
   }
 
   UnloadTexture(Board);
