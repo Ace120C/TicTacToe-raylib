ff8be42a3b16a79dea47fa63cd6bde7c889767fe
Ace120C <145904614+Ace120C@users.noreply.github.com>
2025-05-22 17:27:28 +0100

    I guess you can place a cross now lol

--- main.c
+++ main.c
@@ -1,28 +1,48 @@
 #include <raylib.h>
-#include <stdio.h>
+
+typedef struct {
+  int ZoneXL;
+  int ZoneXR;
+  int ZoneYL;
+  int ZoneYR;
+} ZoneArea;
+
+ZoneArea zone;
+
+ZoneArea box = {309, 485, 256, 490};
+
+bool IsInsideBox(ZoneArea zone, int x, int y)
+{
+  return x >= zone.ZoneXL && x <= zone.ZoneXR &&
+      y >= zone.ZoneYL && y <= zone.ZoneYR ;
+}
 
 int main()
 {
   // Init 
   InitWindow(800, 800, "tictactoe");
   Texture2D Board = LoadTexture("./assets/board.png");
+  Texture2D Cross = LoadTexture("./assets/cross.png");
+  bool PlacedCross = false;
   SetTargetFPS(50);
+
   // Game loop
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     DrawTexture(Board, 0, 0, RAYWHITE);
-    EndDrawing();
-
-    int MousePosX = GetMouseX();
-    int MousePosY = GetMouseY();
-    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-      printf("X: %d\n", MousePosX);
-      printf("Y: %d\n", MousePosY);
+    if (PlacedCross) {
+      DrawTexture(Cross, 315, 290, RAYWHITE);
     }
+    if (IsInsideBox(box, GetMouseX(), GetMouseY()) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      PlacedCross = true;
+    }
+    EndDrawing();
+    
   }
 
   UnloadTexture(Board);
+  UnloadTexture(Cross);
   // Closing Window
   CloseWindow();
   return 0;
